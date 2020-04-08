913a43b4d1d2535ee3a8888502510a6eaca6fba8
Breno Peretta <peretta.breno@gmail.com>
2020-04-08 16:07:04 -0300

    Slider animation and dots of the slider

--- src/pages/Auth/AuthPage.js
+++ src/pages/Auth/AuthPage.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './AuthPage.scss';
-import img from '../../assets/img/Data.png';
 import SingIn from './SingIn/SingIn';
+import Slider from 'infinite-react-carousel';
+import slides from './Slides';
 
 export default function AuthPage(props) {
-   return(
+
+   return (
       <main>
          <section id="slider">
-            <img src={img} alt="Illustration of Invision" />
+            <Slider
+               dots
+               arrows={false}
+               autoplay
+               appendDots={dots => <ul style={{ display: 'block' }}>{dots}</ul>}
+            >
+            {
+               slides.map(slide => (
+                  <div key={slide.id} className="slide">
+                     <img src={slide.img} alt="Illustration of Invision" />
+                     <div>
+                        <h4>{slide.title}</h4>
+                        <p>{slide.text}</p>
+                     </div>
+                  </div>
+               ))
+            }
+            </Slider>
          </section>
          <section id="formInputs">
             <h3 className="title">Invision</h3>
---
+++ src/pages/Auth/Slides.js
@@ -0,0 +1,30 @@
+import img from '../../assets/img/Data.png';
+
+const slides = [
+   {
+      id: 1,
+      img,
+      title: 'Marcenas mattis egestas',
+      text: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.'
+   },
+   {
+      id: 2,
+      img,
+      title: 'Marcenas mattis egestas',
+      text: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.'
+   },
+   {
+      id: 3,
+      img,
+      title: 'Marcenas mattis egestas',
+      text: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.'
+   },
+   {
+      id: 4,
+      img,
+      title: 'Marcenas mattis egestas',
+      text: 'Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.'
+   }
+];
+
+export default slides
