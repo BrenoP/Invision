6b2dc6194e354035208895464671745339ea0571
Breno Peretta <peretta.breno@gmail.com>
2020-04-08 23:44:54 -0300

    Updates

---
+++ precache-manifest.25d6dba129f9c6e5d71eb22011b92802.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df77b4e4c91277d12124d96be7b9c331",
+    "url": "/Invision/index.html"
+  },
+  {
+    "revision": "46112ffb2177b1c1573b",
+    "url": "/Invision/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "cefad5190c1a3a6b404f",
+    "url": "/Invision/static/css/main.06bef5a7.chunk.css"
+  },
+  {
+    "revision": "46112ffb2177b1c1573b",
+    "url": "/Invision/static/js/2.01c78cf4.chunk.js"
+  },
+  {
+    "revision": "05ca525fac8eca843a636d82adf584ed",
+    "url": "/Invision/static/js/2.01c78cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefad5190c1a3a6b404f",
+    "url": "/Invision/static/js/main.4644dab8.chunk.js"
+  },
+  {
+    "revision": "af5abb33eed90f78dac0",
+    "url": "/Invision/static/js/runtime-main.19c51b8e.js"
+  },
+  {
+    "revision": "265643cdbe0a75514d3efcb3f8a2e367",
+    "url": "/Invision/static/media/Data.265643cd.png"
+  },
+  {
+    "revision": "c61c73cdde3c82bb4dba3fa195a965d8",
+    "url": "/Invision/static/media/Google.c61c73cd.svg"
+  }
+]);
