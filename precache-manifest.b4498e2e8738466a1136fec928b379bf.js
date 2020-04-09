b0eb879627a8b531fde7833dc3e709a1c8c298f1
Breno Peretta <peretta.breno@gmail.com>
2020-04-08 23:08:32 -0300

    Updates

---
+++ precache-manifest.b4498e2e8738466a1136fec928b379bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4801b72d49d5b6c8288ae41e0756961d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46112ffb2177b1c1573b",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "cefad5190c1a3a6b404f",
+    "url": "/static/css/main.06bef5a7.chunk.css"
+  },
+  {
+    "revision": "46112ffb2177b1c1573b",
+    "url": "/static/js/2.01c78cf4.chunk.js"
+  },
+  {
+    "revision": "05ca525fac8eca843a636d82adf584ed",
+    "url": "/static/js/2.01c78cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefad5190c1a3a6b404f",
+    "url": "/static/js/main.4644dab8.chunk.js"
+  },
+  {
+    "revision": "581aa33ac755b3fa7332",
+    "url": "/static/js/runtime-main.fe151eb5.js"
+  },
+  {
+    "revision": "265643cdbe0a75514d3efcb3f8a2e367",
+    "url": "/static/media/Data.265643cd.png"
+  },
+  {
+    "revision": "c61c73cdde3c82bb4dba3fa195a965d8",
+    "url": "/static/media/Google.c61c73cd.svg"
+  }
+]);
